b865c40a8f7c90eb176a9d277f428330890a451a
laurenecharney <lauren.e.charney@vanderbilt.edu>
2021-09-30 11:47:29 -0500

    TAP Branding

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
       <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container">
           <Link class="navbar-brand" to="/">
-            React Multi-Page Website
+            The Afterschool Program
           </Link>
           <div>
             <ul class="navbar-nav ml-auto">
