56815982daa1d892eca7bd075f64fc54c765f964
clairemcgonigle <claire.mcgonigle@vanderbilt.edu>
2021-10-26 17:53:50 -0400

    Mentee Page Layout

--- src/components/Mentees.jsx
+++ src/components/Mentees.jsx
@@ -3,17 +3,17 @@ import React from "react";
 function Mentees() {
   return (
     <div className="mentees">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div class="container" style={{ maxWidth: "100%" }}>
+        <div class="row align-items-center" style={{ paddingTop: 35, paddingBottom: 35, backgroundColor: "#f2f2f2" }}>
+          <div class="col-lg-6 offset-1">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
+              src="http://placehold.it/500x300"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Mentees</h1>
+          <div class="col-lg-4">
+            <h1 class="font-weight-light">Our Mentees</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -22,6 +22,104 @@ function Mentees() {
             </p>
           </div>
         </div>
+
+        <div class="row align-items-center my-5">
+          <div class="col-lg-4 offset-2" style={{ alignItems: "center" }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/300x300"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-4">
+            <h2>How Our Mentors Can Help</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.</p>
+          </div>
+        </div>
+
+        <div class="row align-items-center my-5">
+          <div class="col-lg-4 offset-2">
+            <h2>Mentorship Structures</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.</p>
+          </div>
+          <div class="col-lg-4" style={{ alignItems: "center" }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/300x300"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div class="row align-items-center my-5">
+          <div class="col-lg-4 offset-2" style={{ alignItems: "center" }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/300x300"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-4">
+            <h2>Mentee Testimonies</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.</p>
+          </div>
+        </div>
+
+        <div class="row align-items-center" style={{ paddingTop: 35, paddingBottom: 35, backgroundColor: "#f2f2f2" }}>
+          <div class="col-lg-4 offset-2">
+            <h1 class="font-weight-bold" style={{ textAlign: "center" }}>Join Us As a</h1>
+            <h1 class="font-weight-bold" style={{ textAlign: "center" }}>TAP Mentee</h1>
+          </div>
+          <div class="col-lg-4 ">
+            <div class="row">
+              <h3 class="font-weight-semi-bold">Application Process</h3>
+              <p>
+                Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div class="row">
+              <h3 class="font-weight-semi-bold">Why TAP</h3>
+              <p>
+                Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div class="row">
+              <h3 class="font-weight-semi-bold">Join Our Email Chain</h3>
+              <p>
+                Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row align-items-center" style={{ paddingTop: 35, paddingBottom: 35 }}>
+          <div class="col-lg-12" style={{ textAlign: "center" }}>
+            <h3 class="font-weight-bold" style={{ textAlign: "center", paddingBottom: 25 }}>TAP Mentee Application</h3>
+          </div>
+          <div class="col-lg-8 offset-2" style={{ alignItems: "center" }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   );
