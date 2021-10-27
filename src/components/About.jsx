25bc681de02f1965e294c0a1851f19d244722367
clairemcgonigle <claire.mcgonigle@vanderbilt.edu>
2021-10-26 21:23:22 -0400

    About Page Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,47 +6,74 @@ function About() {
     <div className="about">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7" style={{display: 'flex'}}> 
+          <div class="col-lg-6 offset-1" style={{ display: 'flex' }}>
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              width= '600'
+              width='600'
               src={taplogowide}
               alt="TAP Logo"
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
+          <div class="col-lg-4">
+            <h1 class="font-weight-bold">About Us</h1>
+            <h4 class="font-weight-bold">Who We Are.</h4>
             <p>
-            TAP is a collaboration between the Fourteenth Avenue Baptist Church and Vanderbilt University. 
-            Mentors and mentees meet three times a week (Tuesday, Wednesday, and Thursday) for 1 hour.
-            At the moment, TAP is happening virtually via Zoom.
+              TAP is a collaboration between the Fourteenth Avenue Baptist Church and Vanderbilt University.
+              Mentors and mentees meet three times a week (Tuesday, Wednesday, and Thursday) for 1 hour.
+              At the moment, TAP is happening virtually via Zoom.
             </p>
           </div>
         </div>
-        <div class='row'>
-          <div class="col-lg-4">
-            <h2 align = 'center' class="font-weight-light">Exec Board</h2>
-            <h3 class="font-weight-light">Caroline Riedl</h3>
-            <p>
-              Role: Program Director
-              <br></br>
-              Email: caroline.c.riedl@vanderbilt.edu
-            </p>
-            <h3 class="font-weight-light">Sam Suazo</h3>
-            <p>
-              Role: Enrichment Coordinator
-              <br></br>
-              Email: samantha.r.suazo@vanderbilt.edu
-            </p>
-            <h3 class="font-weight-light">Faith Kwentua</h3>
-            <p>
-              Role: Enrichment Coordinator
-              <br></br>
-              Email: faith.kwentua@vanderbilt.edu
-            </p>
+
+        <div class='row align-items-center' style={{paddingBottom: 20, paddingTop: 20}}>
+          <div class="col-lg-6 offset-1" style={{ display: 'flex' }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              width='600'
+              src={taplogowide}
+              alt="TAP Logo"
+            />
           </div>
           <div class="col-lg-4">
-            <h2 align = 'center' class="font-weight-light">The Church</h2>
+            <h1 class="font-weight-bold">About Our Mentors</h1>
+            <p>
+              TAP is a collaboration between the Fourteenth Avenue Baptist Church and Vanderbilt University.
+              Mentors and mentees meet three times a week (Tuesday, Wednesday, and Thursday) for 1 hour.
+              At the moment, TAP is happening virtually via Zoom.
+            </p>
+          </div>
+        </div>
+
+        <div class='row align-items-center' style={{paddingBottom: 20, paddingTop: 20}}>
+          <div class="col-lg-6 offset-1" style={{ display: 'flex' }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              width='600'
+              src={taplogowide}
+              alt="TAP Logo"
+            />
+          </div>
+          <div class="col-lg-4">
+            <h1 class="font-weight-bold">About Our Mentees</h1>
+            <p>
+              TAP is a collaboration between the Fourteenth Avenue Baptist Church and Vanderbilt University.
+              Mentors and mentees meet three times a week (Tuesday, Wednesday, and Thursday) for 1 hour.
+              At the moment, TAP is happening virtually via Zoom.
+            </p>
+          </div>
+        </div>
+
+        <div class='row align-items-center' style={{paddingBottom: 20, paddingTop: 20}}>
+          <div class="col-lg-6 offset-1" style={{ display: 'flex' }}>
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              width='600'
+              src={taplogowide}
+              alt="TAP Logo"
+            />
+          </div>
+          <div class="col-lg-4">
+            <h1 class="font-weight-bold">About The Church</h1>
             <h3 class="font-weight-light">14th Ave Missionary Baptist Church</h3>
             <p>
               Led by Rev. Dr. Frank Gordon
@@ -56,33 +83,93 @@ function About() {
               Nashville, TN 37208
             </p>
           </div>
-          <div class="col-lg-4">
-            <h2 align = 'center' class="font-weight-light">Leadership Teams</h2>
-            <h3 class="font-weight-light">Zoom Team</h3>
-            <p>
-              Jessica Davis, Grace Pulliam, Quinn Muscatel
-            </p>
-            <h3 class="font-weight-light">Family Team</h3>
-            <p>
-              Soretti Donka, Emily Redd, Elizabeth Baum
-            </p>
-            <h3 class="font-weight-light">Activities Team</h3>
-            <p>
-              Matthew McKenna, Mark Wilkins, Victoria King
-            </p>
-            <h3 class="font-weight-light">Communications Team</h3>
-            <p>
-              Madi Sims, Jennifer Fish
-            </p>
+        </div>
+
+        {/* <div class='row align-items-center' style={{paddingBottom: 20, paddingTop: 20}}> */}
+          <div class='row align-items-center' style={{paddingBottom: 20, paddingTop: 20}}>
+            <div class="col-lg-6 offset-1" style={{ display: 'flex' }}>
+              <img
+                class="img-fluid rounded mb-4 mb-lg-0"
+                width='600'
+                src={taplogowide}
+                alt="TAP Logo"
+              />
+            </div>
+            <div class="col-lg-4">
+              <h1 class="font-weight-bold">Meet The Leadership Team</h1>
+            </div>
+          </div>
+
+          <div class="row" style={{ paddingTop: 25, paddingBottom: 25 }}>
+            <div class="col-lg-3 offset-1">
+              <h3 class="font-weight-bold">Caroline Riedl</h3>
+              <h5>
+                <b>Role:</b> Program Director
+                <br></br>
+                <b>Email:</b> caroline.c.riedl@vanderbilt.edu
+              </h5>
+            </div>
+            <div class="col-lg-3 offset-1">
+              <h3 class="font-weight-bold">Sam Suazo</h3>
+              <h5>
+                <b>Role:</b> Enrichment Coordinator
+                <br></br>
+                <b>Email:</b> samantha.r.suazo@vanderbilt.edu
+              </h5>
+            </div>
+            <div class="col-lg-3 offset-1">
+              <h3 class="font-weight-bold">Faith Kwentua</h3>
+              <h5>
+                <b>Role:</b> Enrichment Coordinator
+                <br></br>
+                <b>Email:</b> faith.kwentua@vanderbilt.edu
+              </h5>
+            </div>
+          </div>
+
+        {/* </div> */}
+
+        <div class='row'>
+          <div class="col-lg-8 offset-2" style={{ paddingTop: 25, paddingBottom: 25 }}>
+            <h2 align='center' class="font-weight-semi-bold">Leadership Teams</h2>
+            <div class="row" style={{paddingBottom: 20, paddingTop: 20}}>
+              <div class="col-6" style={{ textAlign: "center" }}>
+                <h3 class="font-weight-semi-bold">Zoom Team</h3>
+                <h5>
+                  Jessica Davis, Grace Pulliam, Quinn Muscatel
+                </h5>
+              </div>
+              <div class="col-6" style={{ textAlign: "center" }}>
+                <h3 class="font-weight-semi-bold">Family Team</h3>
+                <h5>
+                  Soretti Donka, Emily Redd, Elizabeth Baum
+                </h5>
+              </div>
+            </div>
+
+            <div class="row" style={{paddingBottom: 20, paddingTop: 20}}>
+              <div class="col-6" style={{ textAlign: "center" }}>
+                <h3 class="font-weight-semi-bold">Activities Team</h3>
+                <h5>
+                  Matthew McKenna, Mark Wilkins, Victoria King
+                </h5>
+              </div>
+              <div class="col-6" style={{ textAlign: "center" }}>
+                <h3 class="font-weight-semi-bold">Communications Team</h3>
+                <h5>
+                  Madi Sims, Jennifer Fish
+                </h5>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div style={{padding:50}}>
-              <p>.</p>
+        <div style={{ padding: 50 }}>
+          <p>.</p>
         </div>
 
       </div>
-    </div>
+    </div >
   );
 }
 
