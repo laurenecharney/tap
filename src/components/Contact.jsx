c24d4a67197628e018fe0d400057dbe5d9e07f50
clairemcgonigle <claire.mcgonigle@vanderbilt.edu>
2021-10-26 21:29:40 -0400

    Update Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className="contact">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+          <div class="col-lg-5 offset-2">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
@@ -13,19 +13,19 @@ function Contact() {
             />
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">Contact</h1>
-            <p>
-              Email: <a href="mailto:tapintoyoursuccess@gmail.com">tapintoyoursuccess@gmail.com</a>
-            </p>
-            <p>
-              Instagram: <a href="https://www.instagram.com/tapnashville/">@tapnashville</a>
-            </p>
-            <p>
-              Facebook: <a href="https://www.facebook.com/vanderbiltTAP/">The Afterschool Program</a>
-            </p>
-            <p>
-            Faculty Advisor: Ms. Tam 
-            </p>
+            <h1 class="font-weight-bold">Contact Us</h1>
+            <h5 style={{paddingTop: 10, paddingBottom: 10}}>
+              <b>Email:</b> <a href="mailto:tapintoyoursuccess@gmail.com">tapintoyoursuccess@gmail.com</a>
+            </h5>
+            <h5 style={{paddingTop: 10, paddingBottom: 10}}>
+              <b>Instagram:</b> <a href="https://www.instagram.com/tapnashville/">@tapnashville</a>
+            </h5>
+            <h5 style={{paddingTop: 10, paddingBottom: 10}}>
+              <b>Facebook:</b> <a href="https://www.facebook.com/vanderbiltTAP/">The Afterschool Program</a>
+            </h5>
+            <h5 style={{paddingTop: 10, paddingBottom: 10}}>
+              <b>Faculty Advisor:</b> Ms. Tam
+            </h5>
           </div>
         </div>
       </div>
