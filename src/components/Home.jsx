07c7730c6d07c7441ae51c19577024a2ad06a8a6
njwang217 <nicole.j.wang@vanderbilt.edu>
2021-11-10 21:37:17 -0600

    Added testing code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from "react";
+import taplogo from './taplogo.png';
 
 
 {/* GRAPHQL QUERY */}
@@ -45,7 +46,88 @@ function Home() {
   }, []);
 
   if (!page) {
-    return "Loading...";
+    return(
+      <div className="home">
+        <div className="container" style={{ maxWidth: "100%" }}>
+          <div className="row align-items-center" style={{ backgroundColor: "#519dd9", paddingBottom: 40, paddingTop: 40}}>
+            <div className="col-lg-4 offset-2">
+              <img
+                className= "img-fluid rounded mb-4 mb-lg-0"
+                src={taplogo}
+                alt="TAP Logo"
+              />
+            </div>
+            <div className="col-lg-4">
+              <h1 className="font-weight-light" style={{ color: "white"}}>The Afterschool Program</h1>
+              <p style={{ color: "white"}}>
+              Email <a href="mailto:tapintoyoursuccess@gmail.com" style={{color: "white"}}>tapintoyoursuccess@gmail.com</a> to learn more about the overview of 
+              The Afterschool Program. 
+              </p>
+            </div>
+          </div>
+  
+          <div className="row align-items-center " style={{ paddingTop: 40, paddingBottom: 40 }}>
+            <div className="col-lg-4 offset-2">
+              <img
+                className="img-fluid rounded mb-4 mb-lg-0"
+                src={taplogo}
+                width = "400"
+                alt="TAP Logo"
+              />
+            </div>
+            <div className="col-lg-4">
+              <h3 className="font-weight-bold">Our Mission</h3>
+              Email <a href="mailto:tapintoyoursuccess@gmail.com" style={{color: "black"}}>tapintoyoursuccess@gmail.com</a> to learn more about our mission. 
+            </div>
+          </div>
+  
+                  {/* EMBEDDED GOOGLE CALENDAR */}
+            <div className="row" style={{ paddingTop: 35, paddingBottom: 35 }}>
+            <div className="col-lg-8 offset-2">
+              <div style={{ textAlign: "center", paddingBottom: 25 }}>
+                <h1 className="font-weight-bold">Upcoming Events </h1>
+              </div>
+  
+          {/* <div style={{display: 'flex'}}>
+          <Calendar />
+        </div> */}
+              <p style={{textAlign: "center"}}>
+                The calendar currently can not load, please contact us at <a href="mailto:tapintoyoursuccess@gmail.com" style={{color: "black"}}>tapintoyoursuccess@gmail.com</a>
+                <br></br>
+                TAP usually meets from 4-5pm on Mondays, Tuesdays, and Wednesdays.
+                <br></br>
+                Sorry for the inconvenience!
+              </p>
+            </div>
+          </div>
+  
+          {/* VIRTUAL ZOOM LINKS */}
+          <div className="row align-items-center" style={{ paddingTop: 20, paddingBottom: 20, backgroundColor: "#f2f2f2" }}>
+            <div className="col-lg-4 offset-2" style={{ textAlign: "center" }}>
+              <h1 className="font-weight-bold">Join Your Virtual Zoom Session</h1>
+            </div>
+  
+            <div className="col-lg-4" style={{ display: 'flex' }}>
+              <div className="col-md-12">
+                <div className="row" style={{ padding: 20 }}>
+                  <button style={{ background: "#519DD9", color: "#fff", borderRadius: 10, padding: 25, border: 0 }}>Monday</button>
+                </div>
+                <div className="row" style={{ padding: 20 }}>
+                  <button style={{ background: "#519DD9", color: "#fff", borderRadius: 10, padding: 25, border: 0 }}> Tuesday  </button>
+                </div>
+                <div className="row" style={{ padding: 20 }}>
+                  <button style={{ background: "#519DD9", color: "#fff", borderRadius: 10, padding: 25, border: 0 }}>Wednesday </button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div style={{ padding: 40 }}>
+            <p>.</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -94,13 +176,6 @@ function Home() {
             {/* <div style={{display: 'flex'}}>
           <Calendar />
         </div> */}
-            <p style={{textAlign: "center"}}>
-              TAP usually meets from 4-5pm on Mondays, Tuesdays, and Wednesdays.
-              <br></br>
-              If the calendar does not load, please contact us at <a href="mailto:tapintoyoursuccess@gmail.com" style={{color: "black"}}>tapintoyoursuccess@gmail.com</a>
-              <br></br>
-              Sorry for the inconvenience!
-            </p>
           </div>
         </div>
 
