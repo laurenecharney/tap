2977fbb0165fe4415ad4f90a5c6e045b0c4d1050
laurenecharney <lauren.e.charney@vanderbilt.edu>
2021-12-06 11:08:27 -0600

    Fixed Error

--- src/components/Mentees.jsx
+++ src/components/Mentees.jsx
@@ -112,16 +112,8 @@ function Mentees() {
             />
           </div>
           <div className="col-lg-4">
-<<<<<<< HEAD
             <h2>Mentee Testimonies</h2>
             <p>{page.menteeTestimoniesText}</p>
-=======
-            <h2>Testimonies</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.</p>
->>>>>>> d114e6fb0d42bee5143c65a25787686d25c460ca
           </div>
         </div>
 
