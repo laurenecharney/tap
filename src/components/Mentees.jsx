9e22edf6f2b3460de3203c9a4590807d8c7d5ea0
laurenecharney <lauren.e.charney@vanderbilt.edu>
2021-12-06 00:00:12 -0600

    updating heading

--- src/components/Mentees.jsx
+++ src/components/Mentees.jsx
@@ -109,7 +109,7 @@ function Mentees() {
             />
           </div>
           <div className="col-lg-4">
-            <h2>Mentee Testimonies</h2>
+            <h2>Testimonies</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
